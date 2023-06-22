98cd4ca6135e1f8a495b5d042882b3cd6a5fac2a
Crowdbotics <team@crowdbotics.com>
2023-06-22 20:43:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ishutup-41686",
+  url: "https://ishutup-41686.botics.co",
+  api: "https://ishutup-41686.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
